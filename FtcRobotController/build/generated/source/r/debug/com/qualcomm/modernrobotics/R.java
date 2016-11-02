b5b1a31903c1debd8e2cc924ec3e81c84e50ba87
freezone2000 <dev576829@gmail.com>
2016-11-02 13:43:51 -0500

    commit test 2

--- FtcRobotController/build/generated/source/r/debug/com/qualcomm/modernrobotics/R.java
+++ FtcRobotController/build/generated/source/r/debug/com/qualcomm/modernrobotics/R.java
@@ -8,21 +8,21 @@ package com.qualcomm.modernrobotics;
 
 public final class R {
 	public static final class color {
-		public static final int black = 0x7f090000;
-		public static final int bright_red = 0x7f090002;
-		public static final int bright_red_text = 0x7f090003;
-		public static final int dark_red_background = 0x7f090008;
-		public static final int light_red_background = 0x7f09000a;
-		public static final int medium_red_background = 0x7f09000b;
-		public static final int transparent_color = 0x7f09000c;
-		public static final int very_bright_red = 0x7f09000d;
-		public static final int white = 0x7f09000e;
+		public static int black = 0x7f090000;
+		public static int bright_red = 0x7f090002;
+		public static int bright_red_text = 0x7f090003;
+		public static int dark_red_background = 0x7f090008;
+		public static int light_red_background = 0x7f09000a;
+		public static int medium_red_background = 0x7f09000b;
+		public static int transparent_color = 0x7f09000c;
+		public static int very_bright_red = 0x7f09000d;
+		public static int white = 0x7f09000e;
 	}
 	public static final class string {
-		public static final int app_name = 0x7f0a0020;
+		public static int app_name = 0x7f0a0020;
 	}
 	public static final class style {
-		public static final int AppBaseTheme = 0x7f070000;
-		public static final int AppTheme = 0x7f070001;
+		public static int AppBaseTheme = 0x7f070000;
+		public static int AppTheme = 0x7f070001;
 	}
 }
