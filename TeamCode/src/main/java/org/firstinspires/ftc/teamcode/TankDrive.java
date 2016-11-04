dbfc0fdd0a66acd2c6721a3f70da69f65932a719
KDragonM <kennethmitra@gmail.com>
2016-11-03 21:12:10 -0500

    Added Basic Tank Drive - closes #9

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+/**
+ * Created by Kenneth on 11/3/2016.
+ */
+@TeleOp(name = "Tank Drive")
+public class TankDrive extends OpMode {
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+
+    @Override
+    public void init() {
+        leftMotor = hardwareMap.dcMotor.get("leftMotor");
+        rightMotor = hardwareMap.dcMotor.get("rightMotor");
+        rightMotor.setDirection(DcMotor.Direction.REVERSE); //Reverse motor because it is facing other way
+    }
+
+    @Override
+    public void loop() {
+        float rJoyY = -gamepad1.right_stick_y;
+        float lJoyY = -gamepad1.left_stick_y;
+
+        leftMotor.setPower(lJoyY);
+        rightMotor.setPower(rJoyY);
+    }
+}
