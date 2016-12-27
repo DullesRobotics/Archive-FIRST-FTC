288e6cd8bfd5a7563dfc09a8f9c2d298f97cafc9
freezone2000 <dev576829@gmail.com>
2016-12-27 15:51:16 -0600

    Added wheeled shooter to OpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArcadeDriveTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArcadeDriveTeleOp.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.dullesrobotics.ftc.libraries.ArcadeDrive;
 import com.dullesrobotics.ftc.libraries.BasicRobot;
+import com.dullesrobotics.ftc.libraries.WheeledShooter;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -12,15 +13,23 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class ArcadeDriveTeleOp extends OpMode {
     BasicRobot robot;
     ArcadeDrive ArcDrive;
+    WheeledShooter shooter;
     @Override
     public void init() {
         robot = new BasicRobot(hardwareMap.dcMotor.get("FLM"),hardwareMap.dcMotor.get("FRM"),hardwareMap.dcMotor.get("BLM"),hardwareMap.dcMotor.get("BRM"),gamepad1);
         ArcDrive = new ArcadeDrive(robot);
         robot.setDriveTrain(ArcDrive);
+        shooter = new WheeledShooter(1,1);
     }
 
     @Override
     public void loop() {
         robot.driveWithGamepad();
+        if (robot.getGamepad1().a)  //change
+            shooter.shootForward();
+        else if(robot.getGamepad1().b) //change
+            shooter.shootBackward();
+        else if(robot.getGamepad1().x) //change
+            shooter.stop();
     }
 }
