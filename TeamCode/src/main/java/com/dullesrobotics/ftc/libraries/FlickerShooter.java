e84d7b7712261219baffef726691c2f2a5123084
Austin Joseph <awsomeaustin12@gmail.com>
2016-12-27 15:30:38 -0600

    It is the ijhnhnj

---
+++ TeamCode/src/main/java/com/dullesrobotics/ftc/libraries/FlickerShooter.java
@@ -0,0 +1,18 @@
+package com.dullesrobotics.ftc.libraries;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by USER on 12/27/2016.
+ */
+
+
+public class FlickerShooter {
+    private DcMotor ShooterMotor;
+    private D
+    public FlickerShooter() {
+        super();
+    }
+
+
+}
